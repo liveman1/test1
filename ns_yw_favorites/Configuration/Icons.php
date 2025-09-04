3cbeb2e47390af551cd4e48e9a7592e9d39fb8b6
liveman1 <steffen.matzke@gmail.com>
2025-09-04 19:44:41 +0200

    Delete ns_yw_favorites directory

--- ns_yw_favorites/Configuration/Icons.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'ns_yw_favorites-plugin-pi1' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:ns_yw_favorites/Resources/Public/Icons/user_plugin.svg'
-    ],
-];
