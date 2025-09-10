689fcfd168ca8f54b0597beff8aad13a56394b5a
liveman1 <steffen.matzke@gmail.com>
2025-09-10 22:29:25 +0200

    Delete yw_t3sbootstrap_addon directory

--- yw_t3sbootstrap_addon/Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('TYPO3') or die();
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'yw_t3sbootstrap_addon',
-    'Configuration/TypoScript',
-    'yw t3sbootstrap addon'
-);
-
